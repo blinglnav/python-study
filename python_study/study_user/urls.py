ff0047861a42ab8e1a52173412c8aaf45527a317
Min Choro <blinglnav@gmail.com>
2016-11-27 13:39:36 +0900

    user article view 작성

--- python_study/study_user/urls.py
+++ python_study/study_user/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from study_user.views import *
 
 urlpatterns = [
+    url(r'^$', RedirectFirstArticle.as_view(), name='redirect_first_article'),
+    url(r'^(?P<article_id>\w+)/$', ArticleView.as_view(), name='article_view'),
 ]
--- python_study/study_user/views.py
+++ python_study/study_user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404, Http404
+from django.urls import reverse
+from django.views import View
+from study_admin.models import *
 # Create your views here.
+
+class RedirectFirstArticle(View):
+    def get(self, request, *args, **kwargs):
+        article = Article.objects.filter(is_visible=True).order_by('chapter').first()
+        return redirect(reverse('user:article_view', kwargs={'article_id':article.id}))
+
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, pk=kwargs.get('article_id'))
+        if not article.is_visible:
+            raise Http404
+        article_qs = Article.objects.filter(is_visible=True).order_by('chapter')
+        context = {
+            'article': article,
+            'article_list': article_qs,
+        }
+        return render(request, 'user/view.html', context)
