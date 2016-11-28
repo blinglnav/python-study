b7459efac2c26cbe30bcfcca97baf434eecfe129
Min Choro <blinglnav@gmail.com>
2016-11-28 23:57:32 +0900

    일반 유저도 로그인 필요

--- python_study/study_user/views.py
+++ python_study/study_user/views.py
@@ -12,6 +12,8 @@ class RedirectFirstArticle(View):
 
 class ArticleView(View):
     def get(self, request, *args, **kwargs):
+        if not User.now_login(request):
+            return redirect(reverse('admin:login'))
         article = get_object_or_404(Article, pk=kwargs.get('article_id'))
         if not article.is_visible:
             raise Http404
